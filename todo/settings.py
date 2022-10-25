62fa4be51809701beb7d3b13b05e14a62fcc468a
Samuel <onossamuel98@gmail.com>
2022-10-25 15:41:25 +0100

    new commit

--- todo/settings.py
+++ todo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_0x!%-t5#lq+&7*s9t#1@#t%h(64n6!sy79btb7n37oz1c3=p+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['taskdp1.herokuapp.com',"127.0.0.1"]
 
 
 # Application definition
@@ -123,7 +123,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
